5684d51138b86f20926af71d1638616cbc533896
huynhgiao1012 <huynhgiaolethi0@gmail.com>
2024-01-09 21:54:49 +0700

    update BE

--- BE/config/db.js
+++ BE/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 class Mongo {
   gridFs = null;
   static connect = () => {
+    mongoose.set("strictQuery", false);
     mongoose
       .connect(process.env.MONGO_URI)
       .then(() => {
