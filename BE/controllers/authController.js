453358c2b37a038f6c8cce32be4a792c55fdf3ba
huynhgiao1012 <huynhgiaolethi0@gmail.com>
2024-01-17 14:25:31 +0700

    update BE

--- BE/controllers/authController.js
+++ BE/controllers/authController.js
@@ -233,7 +233,7 @@ exports.forgetPassword = catchAsync(async (req, res) => {
     accountId: existEmail._id,
     token: hashToken,
   });
-  const link = `${process.env.FRONT_END_URL}:5500/ResetPassword.html?token=${tokenReset}&userId=${existEmail._id}`;
+  const link = `https://dao-applicationservice.onrender.com/ResetPassword.html?token=${tokenReset}&userId=${existEmail._id}`;
   await EmailService.sendMail(
     process.env.EMAIL,
     email,
